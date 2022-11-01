09af28f2ae01bbb357afc68d71e88468447c3083
Junnosuke Suzuki <ccanmti@gmail.com>
2022-11-01 08:49:42 +0000

    [Finish Bookers2debug]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,14 @@
 class BooksController < ApplicationController
-
+  before_action :correct_user, only: [:edit, :update]
   def show
+    @user = current_user
     @book = Book.find(params[:id])
+    @book1 = Book.new
   end
 
   def index
+    @user = current_user
+    @book = Book.new
     @books = Book.all
   end
 
@@ -12,10 +16,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      flash[:notice]="You have created book successfully."
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -32,15 +37,21 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
