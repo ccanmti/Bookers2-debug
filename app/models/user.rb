4c30cb8fb3a3cb8c0037d39474250bda1dbeb4e9
Junnosuke Suzuki <ccanmti@gmail.com>
2022-11-07 13:30:23 +0000

    [Finish Bookers2debug5]

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,21 @@ def following?(user)
   followings.include?(user)
 end
 
+# 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length:{maximum:50}
 
